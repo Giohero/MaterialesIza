5b40d495adae70013f0f077ec9bc7cdf824b3e11
Giovanni Rodríguez <giovannii@outlook.com>
2021-09-07 09:13:31 -0500

    Creacion Clase y Interfaz:Generic Repositorie

---
+++ MaterialesIza/MaterialesIza/Data/Repositories/GenericRepositorie.cs
@@ -0,0 +1,63 @@
+﻿using MaterialesIza.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialesIza.Data.Repositories
+{
+    public class GenericRepositorie<T> : IGenericRepositorie<T> where T : class, IEntity
+
+    {
+        private readonly DataContext _dataContext;
+
+        public GenericRepositorie(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _dataContext.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dataContext.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task<T> FindByIdAsync(int id)
+        {
+            return await _dataContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dataContext.Set<T>().AddAsync(entity);
+            await SaveAllAsync();
+            return entity;
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> ExistAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _dataContext.SaveChangesAsync() > 0;
+        }
+
+    }
+}
---
+++ MaterialesIza/MaterialesIza/Data/Repositories/IGenericRepositorie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaterialesIza.Data.Repositories
+{
+    interface IGenericRepositorie <T> where T : class
+
+    {
+        IQueryable<T> GetAll();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> FindByIdAsync(int id);
+
+        Task<T> CreateAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task<bool> ExistAsync(int id);
+
+    }
+}
