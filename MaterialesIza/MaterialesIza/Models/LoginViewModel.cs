1be3ebacb57b95a0b6924c42e97c435d99cbb0c7
yair israel zarate fierro <yairfierro_israel01@hotmail.com>
2021-11-03 21:41:28 -0600

    Login AccountController

---
+++ MaterialesIza/MaterialesIza/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaterialesIza.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
