6aafb9807d9afd2a34d4ec389d6afa5025e8ac7a
benjamin rosas beristain <benjaminrosas410@gmail.com>
2021-11-11 11:42:41 -0600

    cambios de ultima hora

--- MaterialesIza/MaterialesIza/Controllers/OrderDetailsController.cs
+++ MaterialesIza/MaterialesIza/Controllers/OrderDetailsController.cs
@@ -56,7 +56,7 @@ namespace MaterialesIza.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Date_Sale,Total_Sale,Iva_Sale,Sales_Remarks")] OrderDetail orderDetail)
+        public async Task<IActionResult> Create(OrderDetail orderDetail)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace MaterialesIza.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Date_Sale,Total_Sale,Iva_Sale,Sales_Remarks")] OrderDetail orderDetail)
+        public async Task<IActionResult> Edit(int id, OrderDetail orderDetail)
         {
             if (id != orderDetail.Id)
             {
--- MaterialesIza/MaterialesIza/Migrations/20211111171154_Initial.Designer.cs
+++ MaterialesIza/MaterialesIza/Migrations/20211111171154_Initial.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MaterialesIza.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20211111094536_Initial")]
+    [Migration("20211111171154_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
