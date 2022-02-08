156e054a08f04de27135830b7b08f11a603aebde
benjamin rosas beristain <benjaminrosas410@gmail.com>
2022-02-07 18:45:33 -0600

    Token

--- MaterialesIza/MaterialesIza/Controllers/API/ProductTypesController.cs
+++ MaterialesIza/MaterialesIza/Controllers/API/ProductTypesController.cs
@@ -7,7 +7,7 @@
     using Microsoft.AspNetCore.Mvc;
     using System.Threading.Tasks;
 
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[Controller]")]
 
     public class ProductTypesController : Controller
