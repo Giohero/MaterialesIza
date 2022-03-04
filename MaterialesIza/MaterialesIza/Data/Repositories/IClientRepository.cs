312c37b2293394f5bead094157ca94cbf7478b10
yair israel zarate fierro <yairfierro_israel01@hotmail.com>
2022-03-04 09:41:59 -0600

    Metodo Clients

--- MaterialesIza/MaterialesIza/Data/Repositories/IClientRepository.cs
+++ MaterialesIza/MaterialesIza/Data/Repositories/IClientRepository.cs
@@ -10,9 +10,9 @@
     {
         IEnumerable<SelectListItem> GetComboClient();
 
-        //IQueryable GetClients();
+        IQueryable GetClients();
 
-        IEnumerable<ClientRequest> GetClients();
+        //IEnumerable<ClientRequest> GetClients();
 
         MaterialesIza.Common.Models.ClientRequest GetClientWithOrdersByEmail(EmailRequest emailClient);
     }
