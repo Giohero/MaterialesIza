35c4f7b3b4fbbe9025451414e720eec9bfcec227
yair israel zarate fierro <yairfierro_israel01@hotmail.com>
2022-03-08 21:52:42 -0600

    Employee Repository

--- MaterialesIza/MaterialesIza/Data/Repositories/EmployeeRepository.cs
+++ MaterialesIza/MaterialesIza/Data/Repositories/EmployeeRepository.cs
@@ -38,65 +38,7 @@ namespace MaterialesIza.Data.Repositories
             return list;
         }
 
-        //public MaterialesIza.Common.Models.EmployeeRequest GetEmployeeWithOrdersByEmail(EmailRequest emailEmployee)
-        //{
-        //    var c = this.dataContext.Employees
-        //        .Include(c => c.User)
-        //        .Include(c => c.Sales)
-        //        .ThenInclude(o => o.SaleDetails)
-        //        .ThenInclude(od => od.Product)
-        //        .ThenInclude(s => s.ProductTypes)
-        //        .Include(c => c.Sales)
-        //        .ThenInclude(c => c.Client)
-        //        .ThenInclude(c => c.User)
-        //        .FirstOrDefault(c => c.User.Email.ToLower() == emailEmployee.Email);
-        //    if (c == null)
-        //    {
-        //        return null;
-        //    }
-        //    var x = new EmployeeRequest
-        //    {
-        //        Id = c.Id,
-        //        FirstName = c.User.FirstName,
-        //        LastName = c.User.LastName,
-        //        Email = c.User.Email,
-        //        PhoneNumber = c.User.PhoneNumber,
-        //        Order = c.Order?.Select(o => new OrderRequest
-        //        {
-        //            Id = o.Id,
-        //            OrderDetails = o.,
-        //            Total_Sale = o.Total_Sale,
-        //            Iva_Sale = o.Iva_Sale,
-        //            Sales_Remarks = o.Sales_Remarks,
-        //            Client = new ClientRequest
-        //            {
-        //                Id = o.Client.Id,
-        //                Email = o.Client.User.Email,
-        //                FirstName = o.Client.User.FirstName,
-        //                LastName = o.Client.User.LastName,
-        //                PhoneNumber = o.Client.User.PhoneNumber,
-        //            },
-        //            Order = o.SaleDetails?.Select(od => new SaleDetailsRequest
-        //            {
-        //                Id = od.Id,
-        //                Quantity = od.Quantity,
-        //                Product = new ProductRequest
-        //                {
-
-        //                    Id = od.Product.Id,
-        //                    Name = od.Product.Name,
-        //                    Price = od.Product.Price,
-        //                    Description = od.Product.Description,
-        //                    ProductTypes = od.Product.ProductTypes.Name
-
-        //                }
-
-        //            })/*.Where(od => od.Date_Sale != null)*/.ToList()
-        //        }).ToList()
-        //    };
-
-        //    return x;
-        //}
+        
         public MaterialesIza.Common.Models.EmployeeRequest GetEmployeeWithOrdersByEmail(EmailRequest emailEmployee)
         {
             var c = this.dataContext.Employees
