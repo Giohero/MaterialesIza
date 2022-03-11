258134e7581ecdc0dec5d197533f775aa7982d7a
benjamin rosas beristain <benjaminrosas410@gmail.com>
2022-03-11 15:48:55 -0600

    Add Select Image

--- MaterialesIza/MaterialesIza.UIForms/MaterialesIza.UIForms.Android/Resources/Resource.designer.cs
+++ MaterialesIza/MaterialesIza.UIForms/MaterialesIza.UIForms.Android/Resources/Resource.designer.cs
@@ -11833,19 +11833,22 @@ namespace MaterialesIza.UIForms.Droid
 			public const int SalesDetail = 2131165342;
 			
 			// aapt resource value: 0x7F07009F
-			public const int setup = 2131165343;
+			public const int select = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int shipping = 2131165344;
+			public const int setup = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int test_custom_background = 2131165345;
+			public const int shipping = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int tooltip_frame_dark = 2131165346;
+			public const int test_custom_background = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int tooltip_frame_light = 2131165347;
+			public const int tooltip_frame_dark = 2131165347;
+			
+			// aapt resource value: 0x7F0700A4
+			public const int tooltip_frame_light = 2131165348;
 			
 			static Drawable()
 			{
